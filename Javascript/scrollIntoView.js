0c6e896b389c55d002f1f0222c8239236d3886b7
gfcrbg <47260227+gfcrbg@users.noreply.github.com>
2020-01-06 10:39:34 -0600

    Create scrollIntoView.js

---
+++ Javascript/scrollIntoView.js
@@ -0,0 +1,20 @@
+Test scrollIntoView
+<br>
+<br>
+<button type="button" onclick="wootA()">Narrative / Text</button>
+<br>
+<br>
+<button type="button" onclick="wootB()">Mulitcheck</button>
+<br>
+<br>
+
+<script>
+function wootA() {
+  var meh = document.getElementById("bleh");
+  meh.scrollIntoView();
+}
+function wootB() {
+  var meh = document.getElementById("boo");
+  meh.scrollIntoView();
+}
+</script>
