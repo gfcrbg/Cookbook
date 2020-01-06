a96b6f229a3efc7efad0309f42db087da2b7802e
gfcrbg <47260227+gfcrbg@users.noreply.github.com>
2020-01-06 11:30:22 -0600

    Update shopReportDates.js

--- Javascript/shopReportDates.js
+++ Javascript/shopReportDates.js
@@ -1,3 +1,16 @@
+/*
+==========
+JavaScript
+==========
+Input A: The user enters the month of the report date. (integer)
+Input B: The user enters the month of the shop date.  (integer)
+Button: The user clicks on button "Compare".  Triggers event that compares integer values in Input A and Input B.
+Output: (boolean logic)  
+  If values are true, "Yes" is the auto-response. 
+  If values are false, "No" is the auto-response. An alert() is triggered that declares "Months do NOT match!".
+*/
+
+
 <left>
                         <h1>Shop / Report Dates</h1>
                         <h3>Determines if the the shop and report dates are in the same month.</h3>
