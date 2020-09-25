9489c8a57bab157433e417568bc3f77719b9a4bf
gfcrbg <47260227+gfcrbg@users.noreply.github.com>
2020-09-25 16:10:42 -0500

    Update Paste.js

--- Google Apps Script/Paste.js
+++ Google Apps Script/Paste.js
@@ -1,9 +1,9 @@
-// copy data from Input to Master Spreadsheet on button click
+// copy data from sheet A and paste to sheet B
 
-function inputPaste() {
+function paste() {
   var ss=SpreadsheetApp.getActive();
-  var sh1=ss.getSheetByName("Input");
-  var sh2=ss.getSheetByName("Master Spreadsheet");
+  var sh1=ss.getSheetByName(" "); // enter the name of the sheet
+  var sh2=ss.getSheetByName(" "); // enther the name of the sheet
 // Shopped Month, Date, Survey Question, Issue, Issue #, Sassie ID, Store Location, Submitter, Concern
   sh1.getRange("A40:S40").copyTo(sh1.getRange("A42:S42"), {contentsOnly:true});
   sh1.getRange("T40:W40").copyTo(sh1.getRange("T42:W42"), {contentsOnly:false});
