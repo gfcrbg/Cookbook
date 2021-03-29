1d0474bf8ad0cd1e8b225cb491dc4174ef6f58de
gfcrbg <47260227+gfcrbg@users.noreply.github.com>
2021-03-29 16:41:57 -0500

    Update sitemaps.py

--- Web Scrapers/sitemaps.py
+++ Web Scrapers/sitemaps.py
@@ -1,24 +1,27 @@
-# python sitemaps.py
+'''
+This will allow you to extract links from the sitemap and/or gather general info, such as number of links.
+Basically, this can be used to query the sitemap.
+'''
 
 import requests
 from bs4 import BeautifulSoup
 
 headers = {''}
 
+# Request
 # enter url
 url = " "
 r = requests.get(url, headers=headers)
 
-# determine parser
+# Parser
+# determine parser, typically either lxml or html
 sp = BeautifulSoup(r.text, 'lxml')
 
 # find 'loc's
 links = sp.find_all('loc')[0:]
  
-
 # print number of links
-# print(len(links))
-
+print(len(links))
 
 # print the links
 for link in links:
