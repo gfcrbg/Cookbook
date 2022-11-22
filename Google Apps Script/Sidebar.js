c59f98ffc6e6ba2f3fb4707cfa07b5cf4406f374
gfcrbg <47260227+gfcrbg@users.noreply.github.com>
2022-11-22 15:27:08 -0600

    Create Sidebar.js

---
+++ Google Apps Script/Sidebar.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+  </head>
+  <body style="background-color:light blue; color:black; font-family:arial">
+    <h1>Instructions</h1>
+    <p>Do <b>NOT</b> use ANY filters.</p>
+    <p>Do <b>NOT</b> edit black columns.</p>
+    <p>You may edit blue columns.</p>
+    <br>
+    <br>
+    <a href="https://github.com/gfcrbg/Cookbook/blob/master/Documentation/Google%20Sheets/Video%20Editing%20Mailer%20Form%20(Responses).md" target="_blank">Documentation</a>
+    <br>
+    <br>
+    <p><b>Sheet Author:</b> Greg Steelman</p>
+  </body>
+</html>
